622f7906d1af2c877c77b0b54c2dd77b4259a7ff
Maaz <mdmaazi2907@gmail.com>
2024-04-13 18:10:23 +0530

    init-setup

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+const dotenv = require('dotenv').config();
+
+import { app } from "./app"
+
+const PORT = process.env.PORT || 8000;
+
+
+app.listen(PORT,()=>{console.log(`Server listening on ${PORT}`)})
